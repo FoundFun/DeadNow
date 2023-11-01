8d5d66b99931e7d3907ddc9efb4c9ee12352e58b
Anatoly Ponomarev <i@foundfun.ru>
2023-11-01 22:04:39 +0500

    Change EnemySpeak text size, add const

--- Assets/CodeBase/Enemy/EnemySpeaker.cs
+++ Assets/CodeBase/Enemy/EnemySpeaker.cs
@@ -6,10 +6,16 @@ namespace CodeBase.Enemy
 {
     public class EnemySpeaker : MonoBehaviour
     {
-        public Text Dead;
-        public Text Tutorial;
+        [SerializeField] private Text _dead;
+        [SerializeField] private Text _tutorial;
+        
+        private const float TargetSize = 0.01f;
+        private const int ShowTime = 4;
 
         private Coroutine _coroutine;
+
+        public Text Dead => _dead;
+        public Text Tutorial => _tutorial;
 
         public void ShowText(Text text)
         {
@@ -21,18 +27,18 @@ namespace CodeBase.Enemy
 
         private IEnumerator OnShowText(Text text)
         {
-            text.gameObject.LeanScale(new Vector3(0.009259259f, 0.009259259f, 0.009259259f), 1).setEaseOutBounce();
+            text.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
 
-            yield return new WaitForSeconds(4);
+            yield return new WaitForSeconds(ShowTime);
 
             text.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
         }
 
         private IEnumerator OnShowTwoText(Text text)
         {
-            text.gameObject.LeanScale(new Vector3(0.009259259f, 0.009259259f, 0.009259259f), 1).setEaseOutBounce();
+            text.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
 
-            yield return new WaitForSeconds(4);
+            yield return new WaitForSeconds(ShowTime);
 
             text.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
         }
