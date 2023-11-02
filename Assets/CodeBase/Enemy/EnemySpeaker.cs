6b6f26e186e1f29e356f63cc7ad2f0bb57891142
Anatoly Ponomarev <i@foundfun.ru>
2023-11-02 13:00:51 +0500

    Change Text to TextMeshProGUI

--- Assets/CodeBase/Enemy/EnemySpeaker.cs
+++ Assets/CodeBase/Enemy/EnemySpeaker.cs
@@ -1,46 +1,46 @@
 ﻿using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace CodeBase.Enemy
 {
     public class EnemySpeaker : MonoBehaviour
     {
-        [SerializeField] private Text _dead;
-        [SerializeField] private Text _tutorial;
+        [SerializeField] private TextMeshProUGUI _dead;
+        [SerializeField] private TextMeshProUGUI _tutorial;
         
         private const float TargetSize = 0.01f;
         private const int ShowTime = 4;
 
         private Coroutine _coroutine;
 
-        public Text Dead => _dead;
-        public Text Tutorial => _tutorial;
+        public TextMeshProUGUI Dead => _dead;
+        public TextMeshProUGUI Tutorial => _tutorial;
 
-        public void ShowText(Text text)
+        public void ShowText(TextMeshProUGUI message)
         {
             if (_coroutine != null)
-                StartCoroutine(OnShowTwoText(text));
+                StartCoroutine(OnShowTwoText(message));
             else
-                _coroutine = StartCoroutine(OnShowText(text));
+                _coroutine = StartCoroutine(OnShowText(message));
         }
 
-        private IEnumerator OnShowText(Text text)
+        private IEnumerator OnShowText(TextMeshProUGUI message)
         {
-            text.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
+            message.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
 
             yield return new WaitForSeconds(ShowTime);
 
-            text.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
+            message.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
         }
 
-        private IEnumerator OnShowTwoText(Text text)
+        private IEnumerator OnShowTwoText(TextMeshProUGUI message)
         {
-            text.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
+            message.gameObject.LeanScale(new Vector3(TargetSize, TargetSize, TargetSize), 1).setEaseOutBounce();
 
             yield return new WaitForSeconds(ShowTime);
 
-            text.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
+            message.gameObject.LeanScale(Vector3.zero, 1).setEaseOutBounce();
         }
     }
 }
