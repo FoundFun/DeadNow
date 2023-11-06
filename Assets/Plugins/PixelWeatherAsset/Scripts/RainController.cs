08f11dc025e537b21644e354d9c08018a215d0cf
Anatoly Ponomarev <i@foundfun.ru>
2023-11-06 22:46:26 +0500

    Remove trash, change the code style of weather controllers

---
+++ Assets/Plugins/PixelWeatherAsset/Scripts/RainController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace PixelWeatherAsset.Scripts
+{
+    public class RainController : MonoBehaviour
+    {
+        [Range(0, 1f)] [SerializeField] private float _masterIntensity = 1f;
+        [Range(0, 1f)] [SerializeField] private float _rainIntensity = 0.7f;
+        [Range(0, 1f)] [SerializeField] private float _windIntensity = 0.5f;
+        [Range(0, 1f)] [SerializeField] private float _fogIntensity = 0.25f;
+        [Range(0, 1f)] [SerializeField] private float _lightningIntensity = 1f;
+
+        [SerializeField] private ParticleSystem _rainPart;
+        [SerializeField] private ParticleSystem _windPart;
+        [SerializeField] private ParticleSystem _lightningPart;
+        [SerializeField] private ParticleSystem _fogPart;
+        [SerializeField] private bool _autoUpdate;
+
+        private ParticleSystem.EmissionModule _rainEmission;
+        private ParticleSystem.ForceOverLifetimeModule _rainForce;
+        private ParticleSystem.EmissionModule _windEmission;
+        private ParticleSystem.MainModule _windMain;
+        private ParticleSystem.EmissionModule _lightningEmission;
+        private ParticleSystem.EmissionModule _fogEmission;
+
+        private void Awake()
+        {
+            _rainEmission = _rainPart.emission;
+            _rainForce = _rainPart.forceOverLifetime;
+            _windEmission = _windPart.emission;
+            _windMain = _windPart.main;
+            _lightningEmission = _lightningPart.emission;
+            _fogEmission = _fogPart.emission;
+            UpdateAll();
+        }
+
+        private void Update()
+        {
+            if (_autoUpdate)
+                UpdateAll();
+        }
+
+        private void UpdateAll()
+        {
+            _rainEmission.rateOverTime = 200f * _masterIntensity * _rainIntensity;
+            _rainForce.x = new ParticleSystem.MinMaxCurve(-25f * _windIntensity * _masterIntensity,
+                (-3 - 30f * _windIntensity) * _masterIntensity);
+            _windEmission.rateOverTime = 5f * _masterIntensity * (_windIntensity + _fogIntensity);
+            _windMain.startLifetime = 2f + 5f * (1f - _windIntensity);
+            _windMain.startSpeed = new ParticleSystem.MinMaxCurve(15f * _windIntensity, 25 * _windIntensity);
+            _fogEmission.rateOverTime =
+                (1f + (_rainIntensity + _windIntensity) * 0.5f) * _fogIntensity * _masterIntensity;
+
+            if (_rainIntensity * _masterIntensity < 0.7f)
+                _lightningEmission.rateOverTime = 0;
+            else
+                _lightningEmission.rateOverTime = _lightningIntensity * _masterIntensity * 0.4f;
+        }
+
+        private void OnMasterChanged(float value)
+        {
+            _masterIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnRainChanged(float value)
+        {
+            _rainIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnWindChanged(float value)
+        {
+            _windIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnLightningChanged(float value)
+        {
+            _lightningIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnFogChanged(float value)
+        {
+            _fogIntensity = value;
+            UpdateAll();
+        }
+    }
+}
---
+++ Assets/Plugins/PixelWeatherAsset/Scripts/SnowController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+namespace PixelWeatherAsset.Scripts
+{
+    public class SnowController : MonoBehaviour
+    {
+        [Range(0, 1f)] [SerializeField] public float _masterIntensity = 1f;
+        [Range(0, 1f)] [SerializeField] public float _snowIntensity = 1f;
+        [Range(0, 1f)] [SerializeField] public float _windIntensity = 1f;
+        [Range(0, 1f)] [SerializeField] public float _fogIntensity = 1f;
+        [Range(0, 7f)] [SerializeField] public float _snowLevel;
+
+        [SerializeField] public ParticleSystem _snowPart;
+        [SerializeField] public ParticleSystem _windPart;
+        [SerializeField] public ParticleSystem _fogPart;
+        [SerializeField] private Material _snowMat;
+        [SerializeField] public bool _autoUpdate;
+
+        private const string SnowLevel = "_SnowLevel";
+
+        private ParticleSystem.EmissionModule _snowEmission;
+        private ParticleSystem.ForceOverLifetimeModule _snowForce;
+        private ParticleSystem.ShapeModule _snowShape;
+        private ParticleSystem.EmissionModule _windEmission;
+        private ParticleSystem.MainModule _windMain;
+        private ParticleSystem.EmissionModule _lightningEmission;
+        private ParticleSystem.MainModule _lightningMain;
+        private ParticleSystem.EmissionModule _fogEmission;
+
+        private void Awake()
+        {
+            _snowEmission = _snowPart.emission;
+            _snowShape = _snowPart.shape;
+            _snowForce = _snowPart.forceOverLifetime;
+            _windEmission = _windPart.emission;
+            _windMain = _windPart.main;
+            _fogEmission = _fogPart.emission;
+            UpdateAll();
+        }
+
+        private void Update()
+        {
+            if (_autoUpdate)
+                UpdateAll();
+        }
+
+        private void UpdateAll()
+        {
+            _snowEmission.rateOverTime = 110f * _masterIntensity * _snowIntensity;
+            _snowShape.radius = 30f * Mathf.Clamp(_windIntensity, 0.4f, 1f) * _masterIntensity;
+            _snowForce.x = new ParticleSystem.MinMaxCurve(-9f * _windIntensity, -3 - 14f * _windIntensity);
+            _windEmission.rateOverTime = 14f * _masterIntensity * (_windIntensity + _fogIntensity);
+            _windMain.startLifetime = 2f + 6f * (1f - _windIntensity);
+            _windMain.startSpeed = new ParticleSystem.MinMaxCurve(15f * _windIntensity, 20 * _windIntensity);
+            _fogEmission.rateOverTime = (_fogIntensity + (_snowIntensity + _windIntensity) * 0.5f) * _masterIntensity;
+            _snowMat.SetFloat(SnowLevel, _snowLevel);
+        }
+
+        private void OnMasterChanged(float value)
+        {
+            _masterIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnSnowChanged(float value)
+        {
+            _snowIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnWindChanged(float value)
+        {
+            _windIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnFogChanged(float value)
+        {
+            _fogIntensity = value;
+            UpdateAll();
+        }
+
+        private void OnSnowLevelChanged(float value)
+        {
+            _snowLevel = value;
+            UpdateAll();
+        }
+    }
+}
