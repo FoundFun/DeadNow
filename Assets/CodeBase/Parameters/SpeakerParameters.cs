6f9e1d4437538d55a358f681e529691ab41aff2e
RadHead600 <Shiestov2014@mail.ru>
2023-11-07 23:51:58 +0300

    add ScriptableObject

---
+++ Assets/CodeBase/Parameters/SpeakerParameters.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpeakerParameters", menuName = "CustomParameters/SpeakerParameters")]
+public class SpeakerParameters : ScriptableObject
+{
+    [SerializeField] private List<TextMeshProUGUI> _startTexts;
+    [SerializeField] private List<TextMeshProUGUI> _dialogTexts;
+
+    public List<TextMeshProUGUI> StartTexts => _startTexts;
+    public List<TextMeshProUGUI> DialogTexts => _dialogTexts;
+}
