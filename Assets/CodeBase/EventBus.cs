e7b7aca94ad8e3ba0540ba7f5a6f7b15d8fe1b81
Anatoly Ponomarev <i@foundfun.ru>
2023-11-09 12:31:32 +0500

    Add event bus

---
+++ Assets/CodeBase/EventBus.cs
@@ -0,0 +1,18 @@
+namespace CodeBase
+{
+    public class EventBus
+    {
+        private static EventBus _instance;
+
+        public static EventBus Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new EventBus();
+
+                return _instance;
+            }
+        }
+    }
+}
